01295f70e0d726c7747fb1bc578dc697364f9c23
Nikola Nemes <nikolanemzi@gmail.com>
2020-12-28 04:53:23 +0200

    Completed part 3

--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
 }
 
 const password = process.argv[2]
@@ -12,7 +12,7 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
 const personSchema = new mongoose.Schema({
     name: String,
     number: String
-  })
+})
   
 const Person = mongoose.model('Person', personSchema)
 
@@ -21,17 +21,17 @@ if (process.argv.length === 5) {
     const person = new Person({
         name: process.argv[3],
         number: process.argv[4]
-    });
-    person.save().then(result => {
-        console.log(`added ${person.name} number ${person.number} to phonebook`);
-        mongoose.connection.close();
-    });
+    })
+    person.save().then(() => {
+        console.log(`added ${person.name} number ${person.number} to phonebook`)
+        mongoose.connection.close()
+    })
 } else {
 // otherwise fetch all people from phonebook
-    console.log('phonebook:');
+    console.log('phonebook:')
     Person.find({}).then(result => {
         result.forEach(person => {
-            console.log(`${person.name} ${person.number}`);
+            console.log(`${person.name} ${person.number}`)
         })
         mongoose.connection.close()
     })
